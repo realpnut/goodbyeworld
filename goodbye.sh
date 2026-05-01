a3c288be46c1b6ef2a0c19436d82d63e089adc1b
realpnut <pnuthacking@gmail.com>
2026-05-01 22:59:12 +0200

    Add files via upload

---
+++ goodbye.sh
@@ -0,0 +1,22 @@
+echo [*] Counting particles in the universe...
+sleep 0.8
+echo [+] Estimated atoms: 10^70
+sleep 0.2
+echo [*] Initializing modules...
+sleep 2
+echo [+] Modules loaded!
+sleep 0.23
+echo [*] Establishing connection to ISS...
+sleep 1.7
+echo [+] Connected!
+sleep 0.4
+echo [*] Connecting to Area 51 mainframe...
+sleep 1.7
+echo [+] Connection established
+sleep 0.5
+echo [*] Downloading alien knowledge...
+sleep 1.3
+echo [+] Data received
+sleep 0.75
+echo [!] Goodbye, world!
+exit
